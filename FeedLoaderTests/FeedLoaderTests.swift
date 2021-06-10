fe84375f65d8db48149823eddc50cad45a3500e6
George Vardikos <g.vardikos@afse.eu>
2021-06-10 23:58:05 +0300

    create the load method and inject the client to the remoteFeedLoader

--- FeedLoaderTests/FeedLoaderTests.swift
+++ FeedLoaderTests/FeedLoaderTests.swift
@@ -9,8 +9,15 @@ import XCTest
 @testable import FeedLoader
 
 class RemoteFeedLoader {
+    var client: HttpClient
 
+    init(httpClient: HttpClient) {
+        self.client = httpClient
+    }
 
+    func load() {
+        client.baseUrl = URL(string: "https//:a-url")!
+    }
 }
 
 class HttpClient {
@@ -21,8 +28,16 @@ class FeedLoaderTests: XCTestCase {
 
     func test_client_urlInNil() {
         let client = HttpClient()
-        _ = RemoteFeedLoader()
+        _ = RemoteFeedLoader(httpClient: client)
         XCTAssertNil(client.baseUrl)
     }
 
+    func test_load_requestDataFromUrl() {
+        let client = HttpClient()
+        let sut = RemoteFeedLoader(httpClient: client)
+
+        sut.load()
+
+        XCTAssertNotNil(client.baseUrl)
+    }
 }
