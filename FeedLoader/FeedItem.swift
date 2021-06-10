a18367320ad6b7345928a1ff540cb97ea69db15e
George Vardikos <g.vardikos@afse.eu>
2021-06-10 23:10:55 +0300

    Add FeedItem

---
+++ FeedLoader/FeedItem.swift
@@ -0,0 +1,15 @@
+//
+//  FeedItem.swift
+//  FeedLoader
+//
+//  Created by George Vardikos on 10/6/21.
+//
+
+import Foundation
+
+struct FeedItem {
+    let id: UUID
+    let description: String?
+    let location: String?
+    let imageUrl: URL
+}
