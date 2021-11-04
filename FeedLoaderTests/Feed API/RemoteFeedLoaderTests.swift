4ce05ec70ec8184e514e3214f192fe4bd50a1606
George Vardikos <giorgosvardikos@gmail.com>
2021-11-04 10:30:55 +0200

    Simplify type declarations with a helper function

--- FeedLoaderTests/Feed API/RemoteFeedLoaderTests.swift
+++ FeedLoaderTests/Feed API/RemoteFeedLoaderTests.swift
@@ -39,7 +39,7 @@ class RemoteFeedLoaderTests: XCTestCase {
     func test_load_deliversErrorOnClient() {
         let (client, sut) = makeSUT(URL(string:"http://something.com")!)
 
-        expect(sut, toCompleteWith: .failure(RemoteFeedLoader.Error.connectivity), when: {
+        expect(sut, toCompleteWith: failure(.connectivity), when: {
             let clientError = NSError(domain: "test", code: 0)
             client.complete(with: clientError)
 
@@ -52,7 +52,7 @@ class RemoteFeedLoaderTests: XCTestCase {
         let samples = [199, 201, 300, 400, 500]
 
         samples.enumerated().forEach { index, code in
-            expect(sut, toCompleteWith: .failure(RemoteFeedLoader.Error.invalidData), when: {
+            expect(sut, toCompleteWith: failure(.invalidData), when: {
                 let data = makeItemsJSON([])
                 client.complete(withStatusCode: code, at: index, data: data)
             })
@@ -64,7 +64,7 @@ class RemoteFeedLoaderTests: XCTestCase {
     func test_load_deliversErrorOn200HttpResponseWithInvalidJSON() {
         let (client, sut) = makeSUT()
 
-        expect(sut, toCompleteWith: .failure(RemoteFeedLoader.Error.invalidData), when: {
+        expect(sut, toCompleteWith: failure(.invalidData), when: {
             let invalidJSON = Data("invalid json".utf8)
             client.complete(withStatusCode: 200, data: invalidJSON)
         })
@@ -111,6 +111,10 @@ class RemoteFeedLoaderTests: XCTestCase {
     }
 
     // MARK: Helpers
+    
+    private func failure(_ error: RemoteFeedLoader.Error) -> RemoteFeedLoader.Result {
+        return .failure(error)
+    }
 
     private func makeSUT(_ url: URL = URL(string:"http://something.com")!, file: StaticString = #file, line: UInt = #line) -> (HttpClientSpy, RemoteFeedLoader){
         let client = HttpClientSpy()
